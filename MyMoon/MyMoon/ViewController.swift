6df4cad5419f3b532c00818fa1c99b4339ffa005
AranApps <aaronfononi@gmail.com>
2024-10-26 14:29:52 +0330

    Add files via upload

---
+++ MyMoon/MyMoon/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  MyMoon
+//
+//  Created by AranApps on 23/10/2024.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_mercury.jpg")
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+
+}
